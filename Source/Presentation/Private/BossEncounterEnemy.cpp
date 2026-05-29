dc236f1e54acc7785ec88abf4ae2adda60665ac2
Michael Bump <roanger@yahoo.com>
2026-05-30 06:56:44 +0800

    Update: Mostly functioning boss, gonna polish it up

---
+++ Source/Presentation/Private/BossEncounterEnemy.cpp
@@ -0,0 +1,180 @@
+#include "BossEncounterEnemy.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+#include "HeartHealthComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "TimerManager.h"
+
+ABossEncounterEnemy::ABossEncounterEnemy()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	HeartHealth = CreateDefaultSubobject<UHeartHealthComponent>(TEXT("HeartHealth"));
+	HeartHealth->MaxHearts = 6;
+	HeartHealth->StartingHearts = 6;
+	HeartHealth->bDestroyOwnerOnDeath = true;
+
+	if (UCharacterMovementComponent* Movement = GetCharacterMovement())
+	{
+		Movement->bOrientRotationToMovement = true;
+		Movement->MaxWalkSpeed = NormalSpeed;
+	}
+}
+
+void ABossEncounterEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AcquireTarget();
+	EnterJumpPhase();
+
+	GetWorldTimerManager().SetTimer(
+		PhaseSwitchTimerHandle,
+		this,
+		&ABossEncounterEnemy::TogglePhase,
+		FMath::Max(PhaseSwitchInterval, KINDA_SMALL_NUMBER),
+		true
+	);
+}
+
+void ABossEncounterEnemy::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	AcquireTarget();
+
+	if (CurrentPhase == EBossPhase::Charging)
+	{
+		UpdateChargePhase(DeltaSeconds);
+	}
+}
+
+void ABossEncounterEnemy::AcquireTarget()
+{
+	if (IsValid(TargetActor))
+	{
+		return;
+	}
+
+	if (bTargetPlayerZero)
+	{
+		TargetActor = UGameplayStatics::GetPlayerPawn(this, 0);
+	}
+}
+
+void ABossEncounterEnemy::TogglePhase()
+{
+	if (CurrentPhase == EBossPhase::Jumping)
+	{
+		EnterChargePhase();
+	}
+	else
+	{
+		EnterJumpPhase();
+	}
+}
+
+void ABossEncounterEnemy::EnterJumpPhase()
+{
+	CurrentPhase = EBossPhase::Jumping;
+
+	GetWorldTimerManager().ClearTimer(ChargeDamageTimerHandle);
+
+	if (UCharacterMovementComponent* Movement = GetCharacterMovement())
+	{
+		Movement->MaxWalkSpeed = NormalSpeed;
+		Movement->StopMovementImmediately();
+	}
+
+	GetWorldTimerManager().ClearTimer(JumpTimerHandle);
+	PerformJump();
+	GetWorldTimerManager().SetTimer(
+		JumpTimerHandle,
+		this,
+		&ABossEncounterEnemy::PerformJump,
+		FMath::Max(JumpInterval, KINDA_SMALL_NUMBER),
+		true
+	);
+}
+
+void ABossEncounterEnemy::EnterChargePhase()
+{
+	CurrentPhase = EBossPhase::Charging;
+
+	GetWorldTimerManager().ClearTimer(JumpTimerHandle);
+
+	if (UCharacterMovementComponent* Movement = GetCharacterMovement())
+	{
+		Movement->MaxWalkSpeed = ChargeSpeed;
+	}
+}
+
+void ABossEncounterEnemy::PerformJump()
+{
+	const FVector RandomOffset = FVector(
+		FMath::FRandRange(-JumpRadius, JumpRadius),
+		FMath::FRandRange(-JumpRadius, JumpRadius),
+		0.0f
+	);
+
+	FVector JumpDirection = RandomOffset.GetSafeNormal2D();
+	if (IsValid(TargetActor))
+	{
+		const FVector TowardPlayer = (TargetActor->GetActorLocation() - GetActorLocation()).GetSafeNormal2D();
+		JumpDirection = (JumpDirection + TowardPlayer * 0.5f).GetSafeNormal2D();
+	}
+
+	const FVector LaunchVelocity = JumpDirection * JumpHorizontalSpeed + FVector(0.0f, 0.0f, JumpVerticalSpeed);
+	LaunchCharacter(LaunchVelocity, true, true);
+}
+
+void ABossEncounterEnemy::UpdateChargePhase(float DeltaSeconds)
+{
+	if (!IsValid(TargetActor))
+	{
+		return;
+	}
+
+	const FVector ToTarget = TargetActor->GetActorLocation() - GetActorLocation();
+	const FVector MoveDirection = ToTarget.GetSafeNormal2D();
+	if (!MoveDirection.IsNearlyZero())
+	{
+		AddMovementInput(MoveDirection, 1.0f);
+		FaceDirection(MoveDirection, DeltaSeconds);
+	}
+
+	const float DistanceToTarget = ToTarget.Size2D();
+	if (DistanceToTarget <= ChargeHitRange)
+	{
+		TryApplyChargeDamage();
+	}
+}
+
+void ABossEncounterEnemy::FaceDirection(const FVector& Direction, float DeltaSeconds)
+{
+	const FRotator DesiredRotation = Direction.Rotation();
+	const FRotator NewRotation = FMath::RInterpTo(GetActorRotation(), DesiredRotation, DeltaSeconds, 12.0f);
+	SetActorRotation(FRotator(0.0f, NewRotation.Yaw, 0.0f));
+}
+
+void ABossEncounterEnemy::TryApplyChargeDamage()
+{
+	if (!IsValid(TargetActor) || GetWorldTimerManager().IsTimerActive(ChargeDamageTimerHandle))
+	{
+		return;
+	}
+
+	UGameplayStatics::ApplyDamage(TargetActor, ChargeDamage, GetController(), this, nullptr);
+
+	GetWorldTimerManager().SetTimer(
+		ChargeDamageTimerHandle,
+		this,
+		&ABossEncounterEnemy::ResetChargeDamageCooldown,
+		FMath::Max(ChargeDamageCooldown, KINDA_SMALL_NUMBER),
+		false
+	);
+}
+
+void ABossEncounterEnemy::ResetChargeDamageCooldown()
+{
+}
---
+++ Source/Presentation/Public/BossEncounterEnemy.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BossEncounterEnemy.generated.h"
+
+UENUM(BlueprintType)
+enum class EBossPhase : uint8
+{
+	Jumping,
+	Charging
+};
+
+UCLASS(Blueprintable)
+class PRESENTATION_API ABossEncounterEnemy : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ABossEncounterEnemy();
+
+	virtual void Tick(float DeltaSeconds) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Boss|Health")
+	TObjectPtr<class UHeartHealthComponent> HeartHealth;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Boss|Phase")
+	float PhaseSwitchInterval = 3.0f;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Boss|Phase")
+	EBossPhase CurrentPhase = EBossPhase::Jumping;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Boss|Jump")
+	float JumpHorizontalSpeed = 650.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Boss|Jump")
+	float JumpVerticalSpeed = 420.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Boss|Jump")
+	float JumpRadius = 350.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Boss|Jump")
+	float JumpInterval = 1.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Boss|Charge")
+	float ChargeSpeed = 750.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Boss|Charge")
+	float NormalSpeed = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Boss|Charge")
+	float ChargeHitRange = 90.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Boss|Combat")
+	float ChargeDamage = 1.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Boss|Combat")
+	float ChargeDamageCooldown = 1.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Boss|Targeting")
+	bool bTargetPlayerZero = true;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Boss|Targeting")
+	TObjectPtr<AActor> TargetActor;
+
+	FTimerHandle PhaseSwitchTimerHandle;
+	FTimerHandle JumpTimerHandle;
+	FTimerHandle ChargeDamageTimerHandle;
+
+	void AcquireTarget();
+	void TogglePhase();
+	void EnterJumpPhase();
+	void EnterChargePhase();
+	void PerformJump();
+	void UpdateChargePhase(float DeltaSeconds);
+	void FaceDirection(const FVector& Direction, float DeltaSeconds);
+	void TryApplyChargeDamage();
+	void ResetChargeDamageCooldown();
+};
